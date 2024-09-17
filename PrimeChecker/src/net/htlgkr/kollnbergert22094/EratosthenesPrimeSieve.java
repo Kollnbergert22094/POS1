a05ce64de4d1c21471c6d4c67a7c1e0acb5e87a1
kollnbergert22094 <kollnbergert22094@sus.htl-grieskirchen.at>
2024-09-17 11:54:38 +0200

    did print Primes

--- PrimeChecker/src/net/htlgkr/kollnbergert22094/EratosthenesPrimeSieve.java
+++ PrimeChecker/src/net/htlgkr/kollnbergert22094/EratosthenesPrimeSieve.java
@@ -1,19 +1,29 @@
 package net.htlgkr.kollnbergert22094;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EratosthenesPrimeSieve implements PrimeSieve{
 
-    private int p;
+
+    private List<Integer> numberList;
+
     public EratosthenesPrimeSieve(int p) {
-        this.p = p;
+        for (int i = 2; i < p; i++) {
+            numberList.add(i);
+        }
+
     }
 
     @Override
     public boolean isPrime(int p) {
-        return false;
+        return numberList.contains(p);
     }
 
     @Override
     public void printPrimes() {
-
+        for (int number : numberList) {
+            if (isPrime(number)) System.out.println(number);
+        }
     }
 }
